2513b7570b70bf52708a4e45654332bc1352dc03
kerenias <kereniasfari98@gmail.com>
2023-10-06 19:17:40 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+/**
+ * main - the function of the program
+ * Description: program that prints the alphabet in lowercase and upper case
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+
+	{
+	if (letter != 'e' && letter != 'q')
+	putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
